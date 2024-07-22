0d1363f3b9d31d162161314643f509380a543568
Nanorasmus <nanorasm466u@gmail.com>
2024-07-22 20:47:22 +0200

    Added checks for Bookkeeper's and Numbers

--- common/src/main/java/me/nanorasmus/nanodev/hex_js/mixin/SpellPatternBlockerC2SMixin.java
+++ common/src/main/java/me/nanorasmus/nanodev/hex_js/mixin/SpellPatternBlockerC2SMixin.java
@@ -1,5 +1,7 @@
 package me.nanorasmus.nanodev.hex_js.mixin;
 
+import at.petrak.hexcasting.api.spell.math.HexAngle;
+import at.petrak.hexcasting.api.spell.math.HexDir;
 import at.petrak.hexcasting.api.spell.math.HexPattern;
 import at.petrak.hexcasting.common.network.MsgNewSpellPatternSyn;
 import me.nanorasmus.nanodev.hex_js.storage.PatternList;
@@ -15,6 +17,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+
 @Mixin(MsgNewSpellPatternSyn.class)
 public class SpellPatternBlockerC2SMixin {
 
@@ -23,20 +27,57 @@ public class SpellPatternBlockerC2SMixin {
 
     @Inject(method = "handle", at = @At("HEAD"), cancellable = true)
     public void handle(MinecraftServer server, ServerPlayerEntity sender, CallbackInfo ci) {
-        PatternList playerPatternList = StorageManager.getPatternList(sender.getUuid());
-        PatternList globalPatternList = StorageManager.getGlobalPatternList();
-        // If it is blocked by HexJS
-        if (playerPatternList.blocks(pattern) || (!playerPatternList.contains(pattern) && globalPatternList.blocks(pattern))) {
-            // Cancel the event
-            sender.sendMessage(Text.of("A strange force is prohibiting me from forming this pattern clearly"));
-            ci.cancel();
+        String signature = pattern.anglesSignature();
+        // Ignore number literals
+        if (!(signature.startsWith("aqaa") || signature.startsWith("dedd"))) {
+            // Ignore Bookkeeper's Gambit
+            boolean isBookKeepers = true;
+
+            ArrayList<HexDir> directions = new ArrayList<>(pattern.directions());
+            HexDir flatDir = pattern.getStartDir();
+            if (!pattern.getAngles().isEmpty() && pattern.getAngles().get(0) == HexAngle.LEFT_BACK) {
+                flatDir = directions.get(0).rotatedBy(HexAngle.LEFT);
+            }
+
+            for (int i = 0; i < directions.size(); i++) {
+                // Angle with respect to the *start direction*
+                var angle = directions.get(i).angleFrom(flatDir);
+                if (angle == HexAngle.FORWARD) {
+                    continue;
+                }
+                if (i >= directions.size() - 1) {
+                    // then we're out of angles!
+                    isBookKeepers = false;
+                    break;
+                }
+                var angle2 = directions.get(i + 1).angleFrom(flatDir);
+                if (angle == HexAngle.RIGHT && angle2 == HexAngle.LEFT) {
+                    i++;
+                    continue;
+                }
+
+                isBookKeepers = false;
+                break;
+            }
+
+            if (!isBookKeepers) {
+                PatternList playerPatternList = StorageManager.getPatternList(sender.getUuid());
+                PatternList globalPatternList = StorageManager.getGlobalPatternList();
+                // If it is blocked by HexJS
+                if (playerPatternList.blocks(pattern) || (!playerPatternList.contains(pattern) && globalPatternList.blocks(pattern))) {
+                    // Cancel the event
+                    sender.sendMessage(Text.of("A strange force is prohibiting me from forming this pattern clearly"));
+                    ci.cancel();
+                }
+                HexPattern playerRedirect = playerPatternList.handleRedirects(pattern);
+                HexPattern globalRedirect = globalPatternList.handleRedirects(pattern);
+                if (playerRedirect != null) {
+                    pattern = playerRedirect;
+                } else if (globalRedirect != null) {
+                    pattern = globalRedirect;
+                }
+            }
         }
-        HexPattern playerRedirect = playerPatternList.handleRedirects(pattern);
-        HexPattern globalRedirect = globalPatternList.handleRedirects(pattern);
-        if (playerRedirect != null) {
-            pattern = playerRedirect;
-        } else if (globalRedirect != null) {
-            pattern = globalRedirect;
-        }
+
     }
 }
